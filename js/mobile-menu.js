d536e93e8e5f5dbcd1d2c41cb20a2fef5060e00e
OlhaKliuchkei <ollisand12@gmail.com>
2022-01-22 15:18:35 +0200

    checked

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,18 @@
 (() => {
     const menuBtnRef = document.querySelector('[data-menu-button]');
     const mobileMenuRef = document.querySelector('[data-menu]');
-    // const logo = document.querySelector('.logo')
-    
+    const body = document.querySelector('#body')
+    const logo = document.querySelector('#logo')
+    const logoColor = document.querySelector('#logo-color')
 
     menuBtnRef.addEventListener('click', () => {
 
         menuBtnRef.classList.toggle('is-open');
         mobileMenuRef.classList.toggle('is-open');
-        // logo.classList.toggle('is-open');
-           
+        body.classList.toggle('is-scroll')
+        logo.classList.toggle('logo-hide')
+        logoColor.classList.toggle('logo-hide')
+      
     });
+
 })();
